7c151e2f5507cab3d14b3b0536aaf7191c660e95
herringblue <ivanckj96@gmail.com>
2017-12-17 17:34:05 +0800

    edit

--- js/scripts.js
+++ js/scripts.js
@@ -113,3 +113,31 @@ const next = () => {
 next();
 
 // ---- //
+
+// scroll to anchor ---- //
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+<!-- click to slide down-->
+$( "#tricks" ).click(function() {
+  $( ".skilllist" ).slideToggle( "slow", function() {
+  });
+});
+
+$( "#blog" ).click(function() {
+  $( ".recentposts" ).slideToggle( "slow", function() {
+  });
+});
+// ---- //
